490c090d9edcb1f57d9b0061cf860f42c659271e
williambasso <50145828+williambasso@users.noreply.github.com>
2019-12-12 13:46:49 -0300

    Update ex1.py
    Update verificação name

--- squad(ex)/ex1.py
+++ squad(ex)/ex1.py
@@ -1,7 +1,3 @@
-#WILLIAM - ISABEL
-
-
-
 # Lista dos programadores
 lista_nomes = ['Nicole', 'Mateus','Tiago']
 
@@ -22,27 +18,28 @@ while len(lista_nomes) > 0:
     # For para exibir os nomes dos programadores
     for i in lista_nomes:
         print(f'\033[1;32m{i}\033[m')
-
     nome = input(f'\033[1;33mNome do funcionário\033[m \n')
 
-    # Opção de banco
-    print('\033[1;33mBanco de dados: [1 - MySQL] [2 - PostgreSQL] [3 - MongoDB]\033[m')
-    banco = input('')
-
-    # Opção de linguagem de programação
-    print('\033[1;36mLinguagem de programação: [1 - PHP] [2 - Java] [3 - Python]\033[m')
-    linguagem = input('')
-
-    # Opção de framework front-end
-    print('\033[1;36mFramework front-end: [1 - React] [2 - Angula] [3 - Vue]\033[m')
-    framework = input('')
-
-    # Adição dos elementos lidos na lista
-    lista.append(linguagem.lower())
-    lista.append(framework.lower())
-    lista.append(banco.lower())
 
     if nome.lower().capitalize() in lista_nomes: 
+
+        # Opção de banco
+        print('\033[1;33mBanco de dados: [1 - MySQL] [2 - PostgreSQL] [3 - MongoDB]\033[m')
+        banco = input('')
+
+        # Opção de linguagem de programação
+        print('\033[1;36mLinguagem de programação: [1 - PHP] [2 - Java] [3 - Python]\033[m')
+        linguagem = input('')
+
+        # Opção de framework front-end
+        print('\033[1;36mFramework front-end: [1 - React] [2 - Angular] [3 - Vue]\033[m')
+        framework = input('')
+        
+        # Adição dos elementos lidos na lista
+        lista.append(linguagem.lower())
+        lista.append(framework.lower())
+        lista.append(banco.lower())
+
         # Verificação do squad da Nicole
         if nome.lower() == 'nicole':
             if lista[0] in nicole and lista[1] in nicole and lista[2] in nicole:
@@ -68,10 +65,10 @@ while len(lista_nomes) > 0:
                 print('\033[1;31mNão se encaixa no squad\033[m')
     else:
         print('\033[1;31mNome inválido\033[m')
-
     # Limpar lista para armazenar os próximos elementos da repetição do while
-    lista = []
 
+    lista = []
+    
     # Verificação dos elesmentos da lista para exibição de mensagem de squads concluídos
     if len(lista_nomes) == 0:
         print(f'\033[1;32mTodos os funcionários se encaixaram nas squads!\033[m')    
